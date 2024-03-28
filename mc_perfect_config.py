1e031d5ca419fa90a48d0ccccb98fb76e44a5e10
xhermit <xhermitone@gmail.com>
2024-03-28 11:49:34 +0700

    Отладка

--- mc_perfect_config.py
+++ mc_perfect_config.py
@@ -20,7 +20,7 @@ except ImportError:
 
 import txtfile_func
 
-__verison__ = (0, 0, 0, 1)
+__verison__ = (0, 0, 2, 1)
 
 DEBUG_MODE = True
 
@@ -214,6 +214,12 @@ regex/\\.[Dd][Bb][Ff]$
     View=dbf_view_dos --dbf-filename=%f
 '''
 
+DELETE_PREV_DBF_VIEWER = '''# dbf
+shell/i/.dbf
+	Open=/usr/lib/mc/ext.d/misc.sh open dbf
+	View=%view{ascii} /usr/lib/mc/ext.d/misc.sh view dbf
+'''
+
 
 def main(*argv):
     """
@@ -232,7 +238,7 @@ def main(*argv):
             # txtfile_func.replaceTextFile(ini_filename, 'pause_after_run=0', 'pause_after_run=2', auto_add=False)
             # info(u'Set pause after run')
             txtfile_func.replaceTextFile(ini_filename, 'skin=default', 'skin=modarin256', auto_add=False)
-            info(u'Set <modarin256> skin')
+            info(u'Set <modarin256> skin in file <%s>' % ini_filename)
 
         menu_filename = os.path.join(home_path, '.config', 'mc', 'menu')
         if not os.path.exists(menu_filename):
@@ -244,42 +250,42 @@ def main(*argv):
         # neofetch / System information
         if not txtfile_func.isInTextFile(menu_filename, NEOFETCH_MENUITEM):
             txtfile_func.appendTextFile(menu_filename, NEOFETCH_MENUITEM)
-            info(u'Add <neofetch> system information')
+            info(u'Add <neofetch> system information in file <%s>' % menu_filename)
 
         # htop / Task monitor
         if not txtfile_func.isInTextFile(menu_filename, HTOP_MENUITEM):
             txtfile_func.appendTextFile(menu_filename, HTOP_MENUITEM)
-            info(u'Add <htop> task monitor')
+            info(u'Add <htop> task monitor in file <%s>' % menu_filename)
 
         # btop / Task monitor
         if not txtfile_func.isInTextFile(menu_filename, BTOP_MENUITEM):
             txtfile_func.appendTextFile(menu_filename, BTOP_MENUITEM)
-            info(u'Add <btop> task monitor')
+            info(u'Add <btop> task monitor in file <%s>' % menu_filename)
 
         # mtr / Traceroute
         if not txtfile_func.isInTextFile(menu_filename, MTR_MENUITEM):
             txtfile_func.appendTextFile(menu_filename, MTR_MENUITEM)
-            info(u'Add <mtr> traceroute tool')
+            info(u'Add <mtr> traceroute tool in file <%s>' % menu_filename)
 
         # lazygit / Git manager
         if not txtfile_func.isInTextFile(menu_filename, GIT_MENUITEM):
             txtfile_func.appendTextFile(menu_filename, GIT_MENUITEM)
-            info(u'Add <git> Git manager')
+            info(u'Add <git> Git manager in file <%s>' % menu_filename)
 
         # python / Python interpreter
         if not txtfile_func.isInTextFile(menu_filename, PY_MENUITEM):
             txtfile_func.appendTextFile(menu_filename, PY_MENUITEM)
-            info(u'Add <python> Python interpreter')
+            info(u'Add <python> Python interpreter in file <%s>' % menu_filename)
 
         # ddgr / Internet searching
         if not txtfile_func.isInTextFile(menu_filename, DDGR_MENUITEM):
             txtfile_func.appendTextFile(menu_filename, DDGR_MENUITEM)
-            info(u'Add <ddgr> Internet searching')
+            info(u'Add <ddgr> Internet searching in file <%s>' % menu_filename)
 
         # lynx / Internet browser
         if not txtfile_func.isInTextFile(menu_filename, LYNX_MENUITEM):
             txtfile_func.appendTextFile(menu_filename, LYNX_MENUITEM)
-            info(u'Add <lynx> Internet browser')
+            info(u'Add <lynx> Internet browser in file <%s>' % menu_filename)
 
         ext_filename = os.path.join(home_path, '.config', 'mc', 'mc.ext')
         if not os.path.exists(ext_filename):
@@ -293,70 +299,83 @@ def main(*argv):
             txtfile_func.replaceTextFile(ext_filename,
                                          src_text=MISC_EXT_SIGNATURE,
                                          dst_text=MISC_EXT_SIGNATURE+os.linesep+LOG_EXT_VIEWER)
-            info(u'Add <log> files viewer')
+            info(u'Add <log> files viewer in file <%s>' % ext_filename)
 
         # PDF files
         if not txtfile_func.isInTextFile(ext_filename, PDF_EXT_VIEWER):
             txtfile_func.replaceTextFile(ext_filename,
                                          src_text=DOC_EXT_SIGNATURE,
                                          dst_text=DOC_EXT_SIGNATURE+os.linesep+PDF_EXT_VIEWER)
-            info(u'Add <pdf> files viewer')
+            info(u'Add <pdf> files viewer in file <%s>' % ext_filename)
 
         # Html files
         if not txtfile_func.isInTextFile(ext_filename, HTML_EXT_VIEWER):
             txtfile_func.replaceTextFile(ext_filename,
                                          src_text=DOC_EXT_SIGNATURE,
                                          dst_text=DOC_EXT_SIGNATURE+os.linesep+HTML_EXT_VIEWER)
-            info(u'Add <html> files viewer')
+            info(u'Add <html> files viewer in file <%s>' % ext_filename)
 
         # Htm files
         if not txtfile_func.isInTextFile(ext_filename, HTM_EXT_VIEWER):
             txtfile_func.replaceTextFile(ext_filename,
                                          src_text=DOC_EXT_SIGNATURE,
                                          dst_text=DOC_EXT_SIGNATURE+os.linesep+HTM_EXT_VIEWER)
-            info(u'Add <htm> files viewer')
+            info(u'Add <htm> files viewer in file <%s>' % ext_filename)
 
         # JSON files
         if not txtfile_func.isInTextFile(ext_filename, JSON_EXT_VIEWER):
             txtfile_func.replaceTextFile(ext_filename,
                                          src_text=DOC_EXT_SIGNATURE,
                                          dst_text=DOC_EXT_SIGNATURE+os.linesep+JSON_EXT_VIEWER)
-            info(u'Add <json> files viewer')
+            info(u'Add <json> files viewer in file <%s>' % ext_filename)
 
         # Docx files
         if not txtfile_func.isInTextFile(ext_filename, DOCX_EXT_VIEWER):
             txtfile_func.replaceTextFile(ext_filename,
                                          src_text=DOC_EXT_SIGNATURE,
                                          dst_text=DOC_EXT_SIGNATURE+os.linesep+DOCX_EXT_VIEWER)
-            info(u'Add <docx> files viewer')
+            info(u'Add <docx> files viewer in file <%s>' % ext_filename)
 
         # Xlsx files
         if not txtfile_func.isInTextFile(ext_filename, XLSX_EXT_VIEWER):
             txtfile_func.replaceTextFile(ext_filename,
                                          src_text=DOC_EXT_SIGNATURE,
                                          dst_text=DOC_EXT_SIGNATURE+os.linesep+XLSX_EXT_VIEWER)
-            info(u'Add <xlsx> files viewer')
+            info(u'Add <xlsx> files viewer in file <%s>' % ext_filename)
 
         # XML files
         if not txtfile_func.isInTextFile(ext_filename, XML_EXT_VIEWER):
             txtfile_func.replaceTextFile(ext_filename,
                                          src_text=DOC_EXT_SIGNATURE,
                                          dst_text=DOC_EXT_SIGNATURE+os.linesep+XML_EXT_VIEWER)
-            info(u'Add <xml> files viewer')
+            info(u'Add <xml> files viewer in file <%s>' % ext_filename)
 
         # Images files
         if not txtfile_func.isInTextFile(ext_filename, IMG_EXT_VIEWER):
             txtfile_func.replaceTextFile(ext_filename,
                                          src_text=IMG_EXT_SIGNATURE,
                                          dst_text=IMG_EXT_SIGNATURE+os.linesep+IMG_EXT_VIEWER)
-            info(u'Add <images> files viewer')
+            info(u'Add <images> files viewer in file <%s>' % ext_filename)
 
         # DBF files
+        if txtfile_func.isInTextFile(ext_filename, DELETE_PREV_DBF_VIEWER):
+            txtfile_func.replaceTextFile(ext_filename,
+                                         src_text=DELETE_PREV_DBF_VIEWER,
+                                         dst_text=os.linesep)
+            warning(u'Delete prev <dbf> files viewer in file <%s>' % ext_filename)
         if not txtfile_func.isInTextFile(ext_filename, DBF_EXT_VIEWER):
             txtfile_func.replaceTextFile(ext_filename,
                                          src_text=MISC_EXT_SIGNATURE,
                                          dst_text=MISC_EXT_SIGNATURE+os.linesep+DBF_EXT_VIEWER)
-            info(u'Add <dbf> files viewer')
+            info(u'Add <dbf> files viewer in file <%s>' % ext_filename)
+
+	# Увеличить масштаб окна dosbox
+        dosbox_conf_filename = os.path.join(home_path, '.dosbox', 'dosbox-0.74-3.conf')
+        if not txtfile_func.isInTextFile(dosbox_conf_filename, 'scaler=normal2x forced'):
+            txtfile_func.replaceTextFile(dosbox_conf_filename,
+                                         src_text='scaler=normal2x',
+                                         dst_text='scaler=normal2x forced')
+            info(u'Set <scaler> X 2 for DosBox in file <%s>' % dosbox_conf_filename)
 
         info(u'... STOP Config Midnight Commander')
     except:
